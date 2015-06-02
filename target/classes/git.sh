a4597e5fc1a9ebbfe4945d800780463acac7a1e5
月光下的光头 <jiege82000@163.com>
2015-06-02 10:28:52 +0800

    初始化项目

--- target/classes/git.sh
+++ target/classes/git.sh
@@ -1,7 +1,7 @@
 cd /Users/xiayingjie/Documents/project/java/auto
 git  add .
+touch README.md
+git add README.md
 git commit -am '初始化项目'
 git remote add origin git@git.oschina.net:xiayingjie/auto.git
 git push -u origin master
-
-git push origin 本地分支:远程分支
